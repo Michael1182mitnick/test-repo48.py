14559b06178f330afd3f13cf629786a73fc701a1
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-08-04 23:15:36 -0800

    Create test-repo48.py

---
+++ test-repo48.py
@@ -0,0 +1,10 @@
+# Write a Python program to create a list of squares of numbers from 1 to n.
+
+new_list = [1, 2, 3, 4, 5]
+
+squared = []
+
+for number in new_list:
+    squared.append(number * number)
+
+print(squared)
